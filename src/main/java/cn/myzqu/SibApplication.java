4bdfbb55b73ba36c15cd8e0ed2ff2abf83e5bb32
guoqiujing <17876253486@163.com>
2018-06-08 14:53:57 +0800

    增加war部署tomcat的支持

--- src/main/java/cn/myzqu/SibApplication.java
+++ src/main/java/cn/myzqu/SibApplication.java
@@ -3,6 +3,7 @@ package cn.myzqu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,5 +14,10 @@ public class SibApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SibApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SibApplication.class);
 	}
 }
