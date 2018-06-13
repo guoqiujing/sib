40f2882fb75f51cea1a0d36dab9d82154218a2dc
xiraly <39379509+xiraly@users.noreply.github.com>
2018-06-13 11:56:21 +0800

    反馈信息管理端

---
+++ src/main/java/cn/myzqu/controller/admin/MessageController.java
@@ -0,0 +1,40 @@
+package cn.myzqu.controller.admin;
+
+import cn.myzqu.dto.PageDTO;
+import cn.myzqu.enums.ResultEnum;
+import cn.myzqu.pojo.Message;
+import cn.myzqu.service.MessageService;
+import cn.myzqu.utils.ResultVOUtil;
+import cn.myzqu.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 反馈信息控制层
+ * Created by xiraly on 2018/6/7.
+ */
+@RestController("AdminMessageController")
+@RequestMapping("admin/message")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    /**
+     * 根据题目id查询所有用户的评论记录
+     * @param page
+     * @param size
+     * @return
+     */
+    @GetMapping("/getList")
+    public Result getMessageByUserId(@RequestParam(value="page",defaultValue = "1") Integer page,
+                                     @RequestParam(value = "size",defaultValue = "10") Integer size){
+        //调用messageService的findAllMessage方法查询出所有用户对该接收者的反馈记录
+        PageDTO data = messageService.findAllMessage(page,size);
+        //若没有就返回错误提示信息
+        if(data==null)  return ResultVOUtil.error(ResultEnum.MESSAGE_EMPTY);
+        //若有数据，则返回成功信息和数据
+        return  ResultVOUtil.success(data.getRows(),data.getTotal());
+    }
+}
